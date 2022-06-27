2a752d1d68c4584531c9e46b50eb0fdafc323f4c
Joe Flateau <joe@joeflateau.net>
2022-06-27 14:44:05 -0400

    feat: override s3 endpoint

--- packages/caliobase/src/object-storage/S3FileProvider.ts
+++ packages/caliobase/src/object-storage/S3FileProvider.ts
@@ -7,10 +7,18 @@ import {
   SignedUploadUrl,
 } from './AbstractFileProvider';
 
-const s3 = new S3({});
-
 export class S3ObjectStorageProvider extends AbstractObjectStorageProvider {
-  constructor(private options: { bucket: string; keyPrefix: string }) {
+  s3 = new S3({
+    endpoint: this.options.endpoint,
+  });
+
+  constructor(
+    private options: {
+      bucket: string;
+      keyPrefix: string;
+      endpoint?: string;
+    }
+  ) {
     super();
   }
 
@@ -24,13 +32,13 @@ export class S3ObjectStorageProvider extends AbstractObjectStorageProvider {
       ContentLength: file.contentLength,
     });
 
-    const result = await getSignedUrl(s3, command, { expiresIn: 15 * 60 });
+    const result = await getSignedUrl(this.s3, command, { expiresIn: 15 * 60 });
 
     return { signedUrl: result };
   }
 
   async deleteFile(path: string): Promise<DeleteResult> {
-    await s3.deleteObject({
+    await this.s3.deleteObject({
       Bucket: this.options.bucket,
       Key: `${this.options.keyPrefix}${path}`,
     });
