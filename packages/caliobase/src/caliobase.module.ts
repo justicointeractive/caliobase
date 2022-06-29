96e1e405a81159402bbe83893589f37806d8839b
Joe Flateau <joe@joeflateau.net>
2022-06-29 15:52:57 -0400

    feat: add an option to disable jwt modules
    so swagger docs can be generated without jwt keys

--- packages/caliobase/src/caliobase.module.ts
+++ packages/caliobase/src/caliobase.module.ts
@@ -56,31 +56,9 @@ const buildInEntities = [
 ];
 
 @Module({
-  imports: [
-    ConfigModule,
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (config: ConfigService) => {
-        const privateKeyBase64 = config.get<string>('JWT_PRIVATE_KEY');
-        if (privateKeyBase64 == null) {
-          throw new Error('could not get private key');
-        }
-        const privateKey = Buffer.from(privateKeyBase64, 'base64').toString(
-          'utf8'
-        );
-        return {
-          privateKey,
-          signOptions: {
-            algorithm: 'RS256',
-          },
-        };
-      },
-    }),
-  ],
+  imports: [ConfigModule],
   providers: [
     AuthService,
-    JwtStrategy,
     MetaService,
     ObjectStorageService,
     {
@@ -111,6 +89,7 @@ export class CaliobaseModule {
     validatorOptions,
     baseUrl,
     emailTransport,
+    disableJwtModules = false,
   }: {
     objectStorageProvider: AbstractObjectStorageProvider;
     socialProviders?: SocialProvider[];
@@ -119,10 +98,41 @@ export class CaliobaseModule {
     validatorOptions?: ValidatorOptions;
     baseUrl: string;
     emailTransport: Transporter;
+    disableJwtModules: boolean;
   }): DynamicModule {
+    const { jwtImports = [], jwtProviders = [] } = disableJwtModules
+      ? {}
+      : {
+          jwtImports: [
+            JwtModule.registerAsync({
+              imports: [ConfigModule],
+              inject: [ConfigService],
+              useFactory: async (config: ConfigService) => {
+                const privateKeyBase64 = config.get<string>('JWT_PRIVATE_KEY');
+                if (privateKeyBase64 == null) {
+                  throw new Error('could not get private key');
+                }
+                const privateKey = Buffer.from(
+                  privateKeyBase64,
+                  'base64'
+                ).toString('utf8');
+
+                return {
+                  privateKey,
+                  signOptions: {
+                    algorithm: 'RS256',
+                  },
+                };
+              },
+            }),
+          ],
+          jwtProviders: [JwtStrategy],
+        };
+
     return {
       module: CaliobaseModule,
       imports: [
+        ...jwtImports,
         ...controllerEntities.map((entity) =>
           createEntityModule(entity, {
             ...CaliobaseModule.defaultValidatorOptions,
@@ -144,6 +154,7 @@ export class CaliobaseModule {
         ]),
       ],
       providers: [
+        ...jwtProviders,
         {
           provide: SocialProvidersToken,
           useValue: socialProviders,
