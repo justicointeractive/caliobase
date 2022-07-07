3cb4c80ea430437db172e3d3a129874c254a138e
Joe Flateau <joe@joeflateau.net>
2022-07-07 16:10:31 -0400

    fix: synchronize example app schema

--- examples/example/test/useSuperTestRequest.ts
+++ examples/example/test/useSuperTestRequest.ts
@@ -1,12 +1,14 @@
 import { INestApplication, ModuleMetadata } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import * as supertest from 'supertest';
+import { DataSource } from 'typeorm';
 
 export function useSuperTestRequest(module: ModuleMetadata) {
   let app: INestApplication;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule(module).compile();
+    await moduleRef.get(DataSource).synchronize();
     app = moduleRef.createNestApplication();
     await app.init();
   });
