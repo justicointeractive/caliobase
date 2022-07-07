0a09c556988a6469022acd08eb91d0faa718e8a4
Joe Flateau <joe@joeflateau.net>
2022-07-07 17:05:15 -0400

    feat: decorate member invitation token with swagger

--- packages/caliobase/src/auth/entities/member-invitation-token.entity.ts
+++ packages/caliobase/src/auth/entities/member-invitation-token.entity.ts
@@ -1,14 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Column, Entity, ManyToOne, PrimaryColumn } from 'typeorm';
 import { Organization } from './organization.entity';
 
 @Entity()
 export class MemberInvitationToken {
   @PrimaryColumn()
+  @ApiProperty()
   token!: string;
 
   @Column()
+  @ApiProperty()
   validUntil!: Date;
 
+  @ApiProperty()
   @ManyToOne(() => Organization)
   organization!: Organization;
 }
