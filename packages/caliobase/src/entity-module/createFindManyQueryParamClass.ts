be868ea03afe693d9ab24cd1a099542a16fcfb10
Joe Flateau <joe@joeflateau.net>
2022-07-20 13:53:13 -0400

    feat: support gt/lt operators on dates

--- packages/caliobase/src/entity-module/createFindManyQueryParamClass.ts
+++ packages/caliobase/src/entity-module/createFindManyQueryParamClass.ts
@@ -1,7 +1,7 @@
 import { Type } from '@nestjs/common';
 import { ApiPropertyOptional } from '@nestjs/swagger';
 import { Type as TransformType } from 'class-transformer';
-import { IsIn, IsNumber, IsOptional, IsString } from 'class-validator';
+import { IsDate, IsIn, IsNumber, IsOptional, IsString } from 'class-validator';
 import {
   Equal,
   FindOperator,
@@ -28,7 +28,7 @@ class Operator<T> {
   public symbol!: string;
   public findOperator!: (value: T) => FindOperator<T>;
   public description!: (name: string, not: boolean) => string;
-  public type!: OperatorType<T>;
+  public types!: OperatorType<T>[];
 
   constructor(operator: Operator<T>) {
     Object.assign(this, operator);
@@ -42,14 +42,14 @@ const filterOperators: Array<Operator<any>> = [
     findOperator: (value) => Equal(value),
     description: (name, not) =>
       `${name} ${not ? 'not equal' : 'equal'} to value`,
-    type: Object,
+    types: [Object],
   }),
   new Operator({
     symbol: 'startsWith',
     findOperator: (value) => Like(`${value}%`),
     description: (name, not) =>
       `${name} ${not ? 'does not start' : 'starts'} with value`,
-    type: String,
+    types: [String],
   }),
   new Operator({
     symbol: 'startsWith.i',
@@ -58,28 +58,28 @@ const filterOperators: Array<Operator<any>> = [
       `${name} ${
         not ? 'does not start' : 'starts'
       } with value (case-insensitive)`,
-    type: String,
+    types: [String],
   }),
   new Operator({
     symbol: 'endsWith',
     findOperator: (value) => Like(`%${value}`),
     description: (name, not) =>
       `${name} ${not ? 'does not end' : 'ends'} with value`,
-    type: String,
+    types: [String],
   }),
   new Operator({
     symbol: 'endsWith.i',
     findOperator: (value) => ILike(`%${value}`),
     description: (name, not) =>
       `${name} ${not ? 'does not end' : 'ends'} with value (case-insensitive)`,
-    type: String,
+    types: [String],
   }),
   new Operator({
     symbol: 'contains',
     findOperator: (value) => Like(`%${value}%`),
     description: (name, not) =>
       `${name} ${not ? 'does not contain' : 'contains'} value`,
-    type: String,
+    types: [String],
   }),
   new Operator({
     symbol: 'contains.i',
@@ -88,40 +88,46 @@ const filterOperators: Array<Operator<any>> = [
       `${name} ${
         not ? 'does not contain' : 'contains'
       } value (case-insensitive)`,
-    type: String,
+    types: [String],
   }),
-  new Operator({
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  new Operator<Number | Date>({
     symbol: 'gt',
     findOperator: (value) => MoreThan(value),
     description: (name, not) =>
       `${name} ${not ? 'not greater' : 'greater'} than`,
-    type: Number,
+    types: [Number, Date],
   }),
-  new Operator({
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  new Operator<Number | Date>({
     symbol: 'gte',
     findOperator: (value) => MoreThanOrEqual(value),
     description: (name, not) =>
       `${name} ${not ? 'not greater' : 'greater'} than or equal to`,
-    type: Number,
+    types: [Number, Date],
   }),
-  new Operator({
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  new Operator<Number | Date>({
     symbol: 'lt',
     findOperator: (value) => LessThan(value),
     description: (name, not) => `${name} ${not ? 'not less' : 'less'} than`,
-    type: Number,
+    types: [Number, Date],
   }),
-  new Operator({
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  new Operator<Number | Date>({
     symbol: 'lte',
     findOperator: (value) => LessThanOrEqual(value),
     description: (name, not) =>
       `${name} ${not ? 'not less' : 'less'} than or equal to`,
-    type: Number,
+    types: [Number, Date],
   }),
+  // eslint-disable-next-line @typescript-eslint/ban-types
   new Operator({
     symbol: 'in',
     findOperator: (values) => In(values),
+    // eslint-disable-next-line @typescript-eslint/ban-types
     description: (name, not) => `${name} ${not ? 'not in' : 'in'}`,
-    type: [Object],
+    types: [[Object]],
   }),
 ];
 
@@ -239,12 +245,16 @@ export function createFindManyQueryParamClass<TEntity>(
 
   keys.forEach(({ key, type: propertyType }) => {
     filterOperators
-      .filter(
-        ({ type: operatorType }) =>
-          extractArrayType(operatorType) === propertyType ||
-          extractArrayType(operatorType) === Object
-      )
-      .forEach(({ symbol: symbol, description, type: operatorType }) => {
+      .map(({ types, ...operator }) => {
+        const operatorType = types.find(
+          (operatorType) =>
+            extractArrayType(operatorType) === propertyType ||
+            extractArrayType(operatorType) === Object
+        );
+        return { ...operator, operatorType };
+      })
+      .filter(({ operatorType }) => operatorType != null)
+      .forEach(({ symbol: symbol, description, operatorType }) => {
         const queryParamName = toQueryParamName(key, symbol);
         const queryParamNotName = toQueryParamName(key, symbol, true);
 
@@ -253,6 +263,8 @@ export function createFindManyQueryParamClass<TEntity>(
         const classTransformerValidatorTypeDecorators =
           propertyType === Number
             ? [TransformType(() => Number, {}), IsNumber({}, { each })]
+            : propertyType === Date
+            ? [TransformType(() => Date, {}), IsDate({ each })]
             : [IsString({ each })];
 
         Reflect.decorate(
