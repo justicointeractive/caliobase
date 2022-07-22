7d3288a06bfc118f0cfd98d262db27636910ad9d
Joe Flateau <joe@joeflateau.net>
2022-07-22 16:12:33 -0400

    test: add test instantiating open id connect provider

---
+++ packages/caliobase/src/auth/social-provider/openid-connect.social-provider.spec.ts
@@ -0,0 +1,21 @@
+import { OpenIdConnectSocialProvider } from './openid-connect.social-provider';
+
+describe('social provider', () => {
+  describe('openid connect', () => {
+    it('should create instance with provider claims', () => {
+      new OpenIdConnectSocialProvider<{ roles?: string[] }>({
+        key: 'test',
+        clientId: 'test',
+        clientSecret: 'test',
+        issuer: 'test',
+        redirectUri: 'https://test.com',
+        mapToMembership: ({ providerTokenClaims }) => ({
+          organizationId: 'test123',
+          role: [
+            providerTokenClaims.roles?.includes('Admin') ? 'owner' : 'guest',
+          ],
+        }),
+      });
+    });
+  });
+});
