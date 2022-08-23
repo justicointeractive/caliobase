fad3edc8b0b4a372a394f1508eb272c58ba84b94
Joe Flateau <joe@joeflateau.net>
2022-08-23 17:28:28 -0400

    chore(tailwindcss-color-css-vars): import code

--- packages/tailwindcss-color-css-vars/src/lib/tailwindcss-color-css-vars.spec.ts
+++
@@ -1,7 +0,0 @@
-import { tailwindcssColorCssVars } from './tailwindcss-color-css-vars';
-
-describe('tailwindcssColorCssVars', () => {
-  it('should work', () => {
-    expect(tailwindcssColorCssVars()).toEqual('tailwindcss-color-css-vars');
-  });
-});
--- packages/tailwindcss-color-css-vars/src/lib/tailwindcss-color-css-vars.ts
+++ packages/tailwindcss-color-css-vars/src/lib/tailwindcss-color-css-vars.ts
@@ -1,3 +1,32 @@
-export function tailwindcssColorCssVars(): string {
-  return 'tailwindcss-color-css-vars';
-}
+import * as plugin from 'tailwindcss/plugin';
+
+type ThemeColors = Record<string, string | Record<string, string>>;
+
+const flattenColorPalette = (colors: ThemeColors): Record<string, string> =>
+  Object.assign(
+    {},
+    ...Object.entries(colors ?? {}).flatMap(([color, values]) =>
+      typeof values == 'object'
+        ? Object.entries(flattenColorPalette(values)).map(([number, hex]) => ({
+            [color + (number === 'DEFAULT' ? '' : `-${number}`)]: hex,
+          }))
+        : [{ [`${color}`]: values }]
+    )
+  );
+
+export const colorVars = plugin(function ({ matchUtilities, theme }) {
+  const colors = flattenColorPalette(theme('fill'));
+
+  matchUtilities(
+    {
+      'color-var': (value: string) => {
+        const [variable, colorRef] = value.split(' ');
+
+        return {
+          [`--${variable}`]: `${colors[colorRef]}`,
+        };
+      },
+    },
+    { type: ['any'] }
+  );
+});
