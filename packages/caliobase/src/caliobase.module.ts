8cbe2b8876e54e9a32e4dce8ae62e2ab0a00c2da
Joe Flateau <joe@joeflateau.net>
2022-08-26 17:36:54 -0400

    fix(caliobase): add a bootstrap method on caliobase module
    to mount stuff that needs to be mounted after nest is set up

--- packages/caliobase/src/caliobase.module.ts
+++ packages/caliobase/src/caliobase.module.ts
@@ -1,4 +1,7 @@
-import { DynamicModule, Module } from '@nestjs/common';
+import { DynamicModule, INestApplication, Module } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Command } from 'commander';
+import { writeFile } from 'fs/promises';
 import {
   CaliobaseAuthModule,
   CaliobaseAuthModuleOptions,
@@ -28,6 +31,36 @@ export type CaliobaseModuleOptions<
 
 @Module({})
 export class CaliobaseModule {
+  async bootstrap(app: INestApplication) {
+    const document = SwaggerModule.createDocument(
+      app,
+      new DocumentBuilder()
+        .setTitle('caliobase')
+        .setDescription('The caliobase app API description')
+        .setVersion('1.0')
+        .addBearerAuth({
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'Bearer',
+          name: 'JWT',
+          description: 'Access Token',
+          in: 'header',
+        })
+        .build()
+    );
+    SwaggerModule.setup('swagger', app, document);
+
+    const { writeSwaggerAndExit } = new Command()
+      .option(`--write-swagger-and-exit [path]`)
+      .parse(process.argv)
+      .opts();
+
+    if (writeSwaggerAndExit) {
+      await writeFile(writeSwaggerAndExit, JSON.stringify(document, null, 2));
+      await app.close();
+    }
+  }
+
   static async forRootAsync<
     TUser extends AbstractUserProfile,
     TOrganization extends AbstractOrganizationProfile
