4e8f7343f7ab6b541285fb9a05de89d9884bab7c
Joe Flateau <joe@joeflateau.net>
2022-09-28 14:24:49 -0400

    fix(caliobase-ui): export pending button

--- packages/caliobase-ui/src/components/index.ts
+++ packages/caliobase-ui/src/components/index.ts
@@ -3,4 +3,5 @@ export * from './ConfirmButton';
 export * from './data';
 export * from './ImageUpload';
 export * from './ModalDialog';
+export * from './PendingButton';
 export * from './TextInput';
