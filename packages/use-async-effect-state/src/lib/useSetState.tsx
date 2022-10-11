1918a106c2d1e0adf031bc3d3cc80f63cdbc553b
Joe Flateau <joe@joeflateau.net>
2022-10-11 13:11:56 -0400

    fix(use-async-effect-state): add a way to pass extra args with setState

--- packages/use-async-effect-state/src/lib/useSetState.tsx
+++ packages/use-async-effect-state/src/lib/useSetState.tsx
@@ -1,16 +1,16 @@
 import { useCallback, useState } from 'react';
 import { useLatestValueRef } from './useLatestValueRef';
 
-export function useSetState<T>(
+export function useSetState<T, TExtraArgs extends any[]>(
   initialState: T | (() => T),
-  beforeSetState: (newValue: T, oldValue: T) => T
+  beforeSetState: (newValue: T, oldValue: T, ...args: TExtraArgs) => T
 ) {
   const [state, _setState] = useState(initialState);
   const beforeSetStateRef = useLatestValueRef(beforeSetState);
   const setState = useCallback(
-    function setState(newValue: T) {
+    function setState(newValue: T, ...args: TExtraArgs) {
       _setState((oldValue) => {
-        return beforeSetStateRef.current(newValue, oldValue);
+        return beforeSetStateRef.current(newValue, oldValue, ...args);
       });
     },
     [beforeSetStateRef]
