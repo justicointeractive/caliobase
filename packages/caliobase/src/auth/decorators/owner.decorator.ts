2fffb7a4271327984d40d04b878294b8c1358f3a
Joe Flateau <joe@joeflateau.net>
2023-05-06 09:19:34 -0400

    fix(caliobase): define orgId column

--- packages/caliobase/src/auth/decorators/owner.decorator.ts
+++ packages/caliobase/src/auth/decorators/owner.decorator.ts
@@ -1,5 +1,5 @@
 import { Type } from '@nestjs/common';
-import { ManyToOne } from 'typeorm';
+import { Column, ManyToOne } from 'typeorm';
 import { Organization } from '../entities/organization.entity';
 
 const ownerKey = 'caliobase:owner';
@@ -12,6 +12,13 @@ export function EntityOwner({
   return function (target, propertyName) {
     Reflect.defineMetadata(ownerKey, propertyName, target.constructor);
     ManyToOne(() => Organization, { eager })(target, propertyName);
+    Reflect.defineMetadata(
+      'design:type',
+      String,
+      target,
+      `${String(propertyName)}Id`
+    );
+    Column({ nullable: true })(target, `${String(propertyName)}Id`);
   };
 }
 
